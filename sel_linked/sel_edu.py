46af13d33a609d5a62b7fa32ca1a0b092a6922b5
caiprozect <caiprozect@gmail.com>
2016-10-02 12:20:35 +0900

    Urls with dir are handled

--- sel_linked/sel_edu.py
+++ sel_linked/sel_edu.py
@@ -28,21 +28,36 @@ pass_field.send_keys(Keys.ENTER)
 
 with open(INFILE, 'r') as urls:
 	for url in urls:
-		print COUNT
 		if 'dir' in url:
 			driver.get(url)
 			try:
-				linked_text_lst = driver.find_elements_by_tag_name("a")
-				for linked_text in linked_text_lst:
-					content = linked_text.find_element_by_xpath("./..").text
-					content_text = content.encode('utf-8')
-					if QUERY in content_text:
-						print content_text
-					else:
-						continue
+				body_class = WebDriverWait(driver, 15).until(
+						EC.presence_of_element_located((By.TAG_NAME, "body"))
+					).get_attribute("class")
 			except:
 				continue
-
+			if "js guest" in body_class.encode('utf8'):
+				result = driver.find_element_by_xpath("//ul[@class='content']")
+				linked_text_lst = result.find_elements_by_tag_name("a")
+				for linked_text in linked_text_lst:
+					content = linked_text.find_element_by_xpath("./..").text
+					content_text = content.encode('utf8')
+					if QUERY in content_text:
+						link = linked_text.get_attribute("href")
+						driver.get(link)
+						break
+					else:
+						continue
+			else:
+				results = driver.find_element_by_id("results")
+				li_lst = results.find_elements_by_xpath("./li")
+				for li in li_lst:
+					desc_text = li.find_element_by_class_name("description").text.encode('utf8')
+					print desc_text
+					if QUERY in desc_text:
+						link = li.find_element_by_tag_name("a").get_attribute("href")
+						driver.get(link)
+						break
 		else:
 			driver.get(url)
 			if "Sign In" in driver.page_source:
@@ -60,6 +75,4 @@ with open(INFILE, 'r') as urls:
 					outfile.write(school_info.encode('utf8'))
 					outfile.write("\n\n\n")
 			except:
-				continue
-
-			
+				continue			
